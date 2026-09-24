9279727f0617c4f4ff93e3f87b34bea91dbf6233
Anna-Ndour <annandour78@gmail.com>
2026-09-24 16:34:17 +0000

    LAB_1

---
+++ registration.ts
@@ -0,0 +1,35 @@
+let courseNames: string[] = [];
+let capacities: number[] = [];
+let enrolledStudents: string[][] = [];
+
+function addCourse(name: string, capacity: number): void {
+  courseNames.push(name);
+  capacities.push(capacity);
+  enrolledStudents.push([]);
+}
+
+function enroll(courseIndex: number, student: string): void {
+  if (enrolledStudents[courseIndex].length >= capacities[courseIndex]) {
+    return;
+  }
+
+  enrolledStudents[courseIndex].push(student);
+}
+
+function printCourse(courseIndex: number): void {
+  console.log(
+    courseNames[courseIndex] + ":",
+    enrolledStudents[courseIndex],
+    "capacity " + capacities[courseIndex]
+  );
+}
+
+addCourse("Object-Oriented Programming", 30);
+addCourse("Databases", 40);
+
+enroll(0, "Awa");
+enroll(0, "Mamadou");
+enroll(1, "Fatou");
+
+printCourse(0);
+printCourse(1);
