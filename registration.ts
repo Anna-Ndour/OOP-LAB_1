26c6332526b6f0fbd2010abf9d55570268c40bbb
Anna-Ndour <annandour78@gmail.com>
2026-09-24 16:57:06 +0000

    Shotgun

--- registration.ts
+++ registration.ts
@@ -24,15 +24,38 @@ function printCourse(courseIndex: number): void {
   );
 }
 
+function importEnrollment(courseIndex: number, student: string): void {
+  enrolledStudents[courseIndex].push(student);
+}
+
+function transferStudent(
+  student: string,
+  fromCourse: number,
+  toCourse: number
+): void {
+  const list = enrolledStudents[fromCourse];
+  const pos = list.indexOf(student);
+
+  if (pos >= 0) {
+    list.splice(pos, 1);
+    enrolledStudents[toCourse].push(student);
+  }
+}
+
+function withdraw(courseIndex: number, student: string): void {
+  const list = enrolledStudents[courseIndex];
+  const pos = list.indexOf(student);
+
+  if (pos >= 0) {
+    list.splice(pos, 1);
+  }
+}
+
 addCourse("Object-Oriented Programming", 30);
 addCourse("Databases", 40);
 
-capacities[0] = -100;
-
 enroll(0, "Awa");
 enroll(0, "Mamadou");
-enroll(0, "Ibrahima");
-
 enroll(1, "Fatou");
 
 printCourse(0);
